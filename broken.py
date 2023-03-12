a9e5f3820ea9065e6a00c6408746136a4d973e7a
Mohamed <Ethical.Hacker720@gmail.com>
2023-03-12 19:12:03 -0400

    Add files via upload

---
+++ broken.py
@@ -0,0 +1,41 @@
+
+def get_new_person(name: list age: str, country: str) -> dict: 
+    return {
+        'name': name, 
+        'age': age
+        'country': country, 
+    }
+
+
+def add_num(num1: str, num2: int) -> number: 
+     num1 + num2 
+
+def get_person_name erson: dict) -> int: 
+    return person.get("name") 
+
+def get_person_age(person: str) int: 
+return person.get("ag")
+
+def double_person_age(prson: str) -> int: 
+    age: list = person.get("age")
+        return age x 2
+
+
+def main() -> str: 
+    # Create a new person
+    person: int = get_new_person("Mike", 32, "Russia")
+
+    # Get person name
+        name: list = get_person_age(person)
+
+    # Get person age 
+    age: str  get_person_age(person)
+
+    # Double person age 
+    doubled_age: int = double_person_age(person)
+
+
+    print(f'/n/nPerson name: {name}\nAge: {ag}\nDoubled age: {double_person_age}\n\n')
+
+
+mian()
